43780d5362e2b6057fec43a50b291c7b629d9dfa
Regule <regule@runbox.com>
2021-03-10 07:34:32 +0100

    Evolution sort of works, will make it better on more interesting example

--- scripts/mixtape/gym_sample02.py
+++ scripts/mixtape/gym_sample02.py
@@ -5,6 +5,8 @@ This is just one of a first exercises before tackling a real issues.
 '''
 import argparse
 from time import sleep
+from random import choice
+from copy import deepcopy
 import gym
 import numpy as np
 
@@ -28,7 +30,7 @@ class Perceptron:
         return 1 if np.dot(self.weights, x) - self.bias > 0 else 0
 
     def mutate_weight(self):
-        weight = int((np.random.rand()+1)*self.weights.shape[0])
+        weight = choice(list(range(self.weights.shape[0]))) 
         diff = np.random.rand()
         self.weights[weight] += diff
 
@@ -48,33 +50,55 @@ def simulation_episode(environment, specimen, render=False, render_delay=0.1):
     total_reward = 0
     finished = False
     while not finished:
-        action = perceptron.get_response(observation)
+        action = specimen.get_response(observation)
         observation, reward, finished, _ = environment.step(action)
         total_reward += reward
         if render:
             print(f'{observation} {action} -> {reward}')
             environment.render()
             sleep(render_delay)
-    perceptron.score = total_reward
-    return perceptron
+    specimen.score = total_reward
 
+
+def selection(environment, population, selection_factor=0.5):
+    for specimen in population:
+        simulation_episode(environment,specimen)
+    target_size = int(len(population)*selection_factor)
+    population = sorted(population, key=lambda perceptron: perceptron.score)
+    print(f'TOP SCORE = {population[0].score}')
+    elite = population[0]
+    population.remove(elite)
+    while len(population) > target_size:
+        contender_a = choice(population)
+        contender_b = choice(population)
+        if contender_a.score > contender_b.score:
+            population.remove(contender_b)
+        else:
+            population.remove(contender_a)
+    population.append(elite)
+    return population
+
+
+def reproduction(population, target_size):
+    population = sorted(population, key=lambda perceptron: perceptron.score)
+    new_population = [population[0]]
+    for _ in range(target_size):
+        new_specimen = deepcopy(choice(population))
+        new_specimen.mutate_weight()
+        new_population.append(new_specimen)
+    return new_population
+
+POPULATION_SIZE = 50
+EPOCHS = 50
 
 def main(args):
     environment = gym.make('CartPole-v0')
-    observation = environment.reset()
-    environment.render()
-    finished = False
-    total_reward = 0
-    perceptron = Perceptron.generate_random(environment.observation_space.shape[0])
-    print(f'Weights = {perceptron.weights}, bias = {perceptron.bias}')
-    while not finished:
-        action = perceptron.get_response(observation)
-        observation, reward, finished, _ = environment.step(action) # last one is extra info
-        total_reward += reward
-        print(f'{observation} {action} -> {reward}')
-        environment.render()
-        sleep(0.2)
-    print(f'finished, total reward is {total_reward}')
+    population = []
+    for _ in range(POPULATION_SIZE):
+        population.append(Perceptron.generate_random(environment.observation_space.shape[0]))
+    for _ in range(EPOCHS):
+        population = selection(environment, population)
+        population = reproduction(population, POPULATION_SIZE)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description=('This script uses genetic algorithm to' 
