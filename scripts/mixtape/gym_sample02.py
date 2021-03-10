c5025f5f90d6a16e889e1dd134af95096d817ec9
Regule <regule@runbox.com>
2021-03-10 13:00:46 +0100

    Cleaned up code

--- scripts/mixtape/gym_sample02.py
+++ scripts/mixtape/gym_sample02.py
@@ -11,6 +11,9 @@ import gym
 import numpy as np
 
 class Perceptron:
+    '''
+    This class represents a simple perceptron like neuron.
+    '''
 
     def __init__(self, weights, bias):
         '''
@@ -25,24 +28,54 @@ class Perceptron:
         self.bias = bias
         self.score = 0
 
-    def get_response(self, x):
-        #print(f'{np.dot(self.weights, x)}')
-        return 1 if np.dot(self.weights, x) - self.bias > 0 else 0
+    def get_response(self, network_input):
+        '''
+        An actual neuron signal processing.
+        :return Neuron response for given input
+        '''
+        return 1 if np.dot(self.weights, network_input) - self.bias > 0 else 0
 
     def mutate_weight(self):
-        weight = choice(list(range(self.weights.shape[0]))) 
+        '''
+        This function add a random value from range [-1,1] to a random weight of neuron.
+        '''
+        weight = choice(list(range(self.weights.shape[0])))
         diff = np.random.rand()
         self.weights[weight] += diff
 
     def mutate_bias(self):
+        '''
+        This function adds a random value from range [-1,1] to bias.
+        '''
         self.bias += np.random.rand()
 
     @staticmethod
     def generate_random(input_size):
+        '''
+        This function generate random perceptron with weight vector of size fitting given
+        input size. All weights as well as the bias will be set to a random value from range
+        [-1,1].
+
+        :param input_size: A size of input that neuron is expected to process
+
+        :return A perceptron cabable of processing input of given size.
+        '''
         return Perceptron(np.random.rand(input_size), np.random.rand())
 
 
 def simulation_episode(environment, specimen, render=False, render_delay=0.1):
+    '''
+    This function runs a Ai Gym simulation episode for a single specimen and assign it
+    score equal to total reward.
+
+    :param environment: Gym environment that will be used for simulation
+    :param specimen: Peceptron that will control agent in simulation, it will be modified by
+    setting new score
+    :param render: If set to true simualtion will be visualised and details will be printed
+    to terminal
+    :param render_delay: Time between steps during visualisation, if render is set to false
+    it has no effect on execution.
+    '''
     observation = environment.reset()
     if render:
         environment.render()
@@ -61,6 +94,18 @@ def simulation_episode(environment, specimen, render=False, render_delay=0.1):
 
 
 def selection(environment, population, selection_factor=0.5):
+    '''
+    This function runs a tournament selection algorithm. In it a two random specimens from
+    population are chosen an the one with lower score is removed. If scores are equal or
+    firs specimen is removed and if both randoms return same object it will be removed.
+
+    :param environment: Gym environment that will be used for simulation
+    :param population: List containing all specimens
+    :param selection_factor: How many specimens should survive selection, value should be set
+    between 0 and 1
+
+    :return A population with some specimens removed by a selection
+    '''
     for specimen in population:
         simulation_episode(environment,specimen)
     target_size = int(len(population)*selection_factor)
@@ -80,6 +125,15 @@ def selection(environment, population, selection_factor=0.5):
 
 
 def reproduction(population, target_size):
+    '''
+    This function implements asexual reproduction in which a random specimens from population
+    are selected, then copied and mutated.
+
+    :param population: All specimens
+    :param target_size: Size of generated population
+
+    :return New population that is a result of reproduction process.
+    '''
     population = sorted(population, key=lambda perceptron: perceptron.score)
     new_population = [population[0]]
     for _ in range(target_size):
@@ -88,21 +142,39 @@ def reproduction(population, target_size):
         new_population.append(new_specimen)
     return new_population
 
-POPULATION_SIZE = 50
-EPOCHS = 50
 
 def main(args):
+    '''
+    This is main function, it runs a evalutionary algorithm
+
+    :param args: aruments from command line
+    '''
     environment = gym.make('CartPole-v0')
     population = []
-    for _ in range(POPULATION_SIZE):
+    for _ in range(args.population_size):
         population.append(Perceptron.generate_random(environment.observation_space.shape[0]))
-    for _ in range(EPOCHS):
+    for _ in range(args.epochs):
         population = selection(environment, population)
-        population = reproduction(population, POPULATION_SIZE)
+        population = reproduction(population, args.population_size)
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description=('This script uses genetic algorithm to' 
+    '''
+    This is function used for parsing command line arguments.
+
+    :return Python namespace containing namespace with argumants.
+    '''
+    parser = argparse.ArgumentParser(description=('This script uses genetic algorithm to'
     'evolve a perceptron that will balance cart pole.'))
+    parser.add_argument('-p',
+            '--population_size',
+            type=int,
+            default=100,
+            help='Number of specimens in a population')
+    parser.add_argument('-e',
+            '--epochs',
+            type=int,
+            default=100,
+            help='Numbers of generations for which algorithm will be run')
     return parser.parse_args()
 
 
