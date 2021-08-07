cba0e0fbd2a1137c70d55be0d07bcc27371d828f
Regule <regule@runbox.com>
2021-08-07 10:47:39 +0200

    Added callbacks to example

--- code/scripts/mixtape/c_python_test.py
+++ code/scripts/mixtape/c_python_test.py
@@ -1,4 +1,4 @@
-from ctypes import c_double, c_int, CDLL
+from ctypes import c_double, c_int, CDLL, CFUNCTYPE, POINTER
 import sys
 import numpy as np
 
@@ -7,6 +7,17 @@ basic_function_lib = CDLL(lib_path)
 
 python_c_square = basic_function_lib.c_python_test_square
 python_c_square.restype = None # Because it returns None (void)
+
+
+def get_percent(percent):
+    """Print advancement and set the next call when C has advanced a further 20%"""
+    print(f'Advancement of C calculations: {percent*100}')
+    return percent + 0.2
+
+CB_FTYPE_DOUBLE_DOUBLE = CFUNCTYPE(c_double, c_double) # define C pointer to a function type
+cb_get_percent = CB_FTYPE_DOUBLE_DOUBLE(get_percent) # define a C function equivalent to the python function "get_percent"
+basic_function_lib.assign_progress_printer_callback(cb_get_percent)  # the the C code about that C function
+
 
 def do_square_using_c(list_in):
     """Call C function to calculate squares"""
