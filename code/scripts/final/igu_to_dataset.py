41be283105d9033959c55c785ef0d7b2b23a2ef0
Regule <regule@runbox.com>
2021-10-03 15:57:45 +0200

    Did nothing

---
+++ code/scripts/final/igu_to_dataset.py
@@ -0,0 +1,20 @@
+'''
+This script transforms preprocessed CSV files generated from SP3.
+'''
+import argparse
+import os
+
+
+
+def main(args):
+    pass
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description=__doc__)
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    main(parse_arguments())
+
+
