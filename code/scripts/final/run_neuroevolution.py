f93a5992376d1ac3b79864d742e7eed2bbf2f43e
Regule <regule@runbox.com>
2021-10-10 12:00:05 +0200

    Basically nothing

--- code/scripts/final/run_neuroevolution.py
+++ code/scripts/final/run_neuroevolution.py
@@ -12,6 +12,12 @@ import numpy as np
 import pandas as pd
 
 
+def prepare_datasets(data_folder, window_size, separator):
+    for root, _, files in os.walk(args.data_folder):
+        for file in files:
+            if '.csv' in file:
+                single_satellite = pd.read_csv(os.path.join(root, file), sep=separator)
+
 def main(args):
     pass
 
@@ -28,6 +34,8 @@ def parse_arguments():
             help='Root folder for all generated outputs.')
     parser.add_argument('-s', '--single_network_mode', action='store_true',
             help='If set only a single network is generated for all clocks.')
+    parser.add_argument('--separator', type=str, default=';',
+            help='Character used for column separation in csv file.')
     return parser.parse_args()
 
 if __name__ == '__main__':
