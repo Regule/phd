19ef1d9d97a460b58ee2fd8277df7b2fb3050028
regule <regule@runbox.com>
2021-11-01 12:24:12 +0100

    Started to work a little

--- code/mixtape/neuroevo-full/neuroevo_full.py
+++ code/mixtape/neuroevo-full/neuroevo_full.py
@@ -7,7 +7,25 @@ import gym
 import argparse
 
 
+def run_simulation(population, config):
+    environment = gym.make(config.environment)
+    top_genome = None
 
+    for genome_id, genome in population:
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        genome.fitness = 0.0
+        observation = environment.reset()
+        finished = False
+        for _ in range(500):
+            reaction = net.activate(observation) 
+            observation, reward, finished, _ = environment.step(reaction)
+            genome.fitness += reward
+            if finished:
+                genome.fitness = -1000.0
+                break
+        if top_genome is None or genome.fitness > top_genome.fitness:
+            top_genome = genome
+    print(f'Best specimen fitness is {top_genome.fitness}')
 
 def main(args):
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
@@ -17,6 +35,10 @@ def main(args):
     print('CONFIGURATION')
     for key, value in config.__dict__.items():
         print(f'{key} --> {value}')
+    population = neat.Population(config)
+    winner = population.run(run_simulation, 100)
+    #show_agent_behaviour(winner, 500, config)
+    #draw_net(config, winner, True)
 
 
 def parser_arguments():
