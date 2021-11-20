14f7f2ebfb9ec8446b52d8537252a44ab2368b43
Regule <regule@runbox.com>
2021-11-20 15:35:05 +0100

    Added simple test

---
+++ RestrictedNeat/scripts/resneat/tests/run_neat_demo.py
@@ -0,0 +1,12 @@
+'''
+This scripts allows a execution of a simple run of Pyneat with our libraries as a 
+overseer.
+It should be ran to manually test results generated.
+'''
+
+import neat
+from resneat.backend.signal_processing import SignalPreprocessingUnit
+from resneat.backend.overseer import Overseer
+
+o = Overseer('BipedalWalker-v3', SignalPreprocessingUnit(), neat, 400)
+o.run_neuroevolution('configs/walker.txt', 10, [neat.StdOutReporter(True)])
