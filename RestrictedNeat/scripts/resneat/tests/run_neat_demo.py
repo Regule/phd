1bcbe3dc90d1360b84c00f9773dd970930959397
Regule <regule@runbox.com>
2021-11-21 11:01:55 +0100

    Started work on data visualisation

--- RestrictedNeat/scripts/resneat/tests/run_neat_demo.py
+++ RestrictedNeat/scripts/resneat/tests/run_neat_demo.py
@@ -7,6 +7,9 @@ It should be ran to manually test results generated.
 import neat
 from resneat.backend.signal_processing import SignalPreprocessingUnit
 from resneat.backend.overseer import Overseer
+from resneat.backend.visualisations import plot_statistics 
 
 o = Overseer('BipedalWalker-v3', SignalPreprocessingUnit(), neat, 400)
-o.run_neuroevolution('configs/walker.txt', 10, [neat.StdOutReporter(True)])
+stats = neat.StatisticsReporter()
+o.run_neuroevolution('configs/walker.txt', 10, [neat.StdOutReporter(True), stats])
+plot_statistics(stats, 'Name', True, 'local/test.png', 'local/test.csv')
