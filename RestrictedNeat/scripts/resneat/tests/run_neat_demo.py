113b148a050e53e342bf2b8b777dbd7b645da491
regule <regule@runbox.com>
2021-11-27 11:20:21 +0100

    Updates to setup.py

--- RestrictedNeat/scripts/resneat/tests/run_neat_demo.py
+++ RestrictedNeat/scripts/resneat/tests/run_neat_demo.py
@@ -9,9 +9,12 @@ from resneat.backend.signal_processing import SignalPreprocessingUnit
 from resneat.backend.overseer import Overseer
 from resneat.utils.visualisations import plot_statistics, plot_speciation 
 
-o = Overseer('BipedalWalker-v3', SignalPreprocessingUnit(), neat, 400)
-stats = neat.StatisticsReporter()
-o.run_neuroevolution('configs/walker.txt', 10, [neat.StdOutReporter(True), stats])
-plot_statistics(stats, 'Name', True, 'local/test.png', 'local/test.csv')
-plot_speciation(stats, 'Name', True, 'local/spec.png', 'local/spec_.csv')
+def main():
+    o = Overseer('BipedalWalker-v3', SignalPreprocessingUnit(), neat, 400)
+    stats = neat.StatisticsReporter()
+    o.run_neuroevolution('configs/walker.txt', 10, [neat.StdOutReporter(True), stats])
+    plot_statistics(stats, 'Name', True, 'local/test.png', 'local/test.csv')
+    plot_speciation(stats, 'Name', True, 'local/spec.png', 'local/spec_.csv')
 
+if __name__ == '__main__':
+    main()
--- RestrictedNeat/setup.py
+++ RestrictedNeat/setup.py
@@ -6,13 +6,20 @@ setup(name='RestrictedNeat',
       author='Piotr Gnyś',
       author_email='regule@runbox.com',
       package_dir = {'': 'scripts'},
-      packages=['resneat.backend', 'resneat.utils'],
+      packages=['resneat.backend', 'resneat.utils', 'resneat.tests'],
       install_requires=[
           'clang==5.0',
           'Box2D==2.3.10',
           'pandas==1.3.3',
           'pyneat==0.1.3',
-          'numpy==1.19.5'
-          ]
+          'numpy==1.19.5',
+          'gym==0.21.0',
+          'matplotlib==3.5.0',
+          'neat-python==0.92'
+          ],
+      entry_points={
+          'console_scripts': ['restneat_test_classic_neat=resneat.tests.run_neat_demo:main']
+          },
+      setup_requires=['flake8']
      )
 
