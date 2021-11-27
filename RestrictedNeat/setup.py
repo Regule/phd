3f9c053a030a048117ce5a71627ecf8a5a012c29
regule <regule@runbox.com>
2021-11-27 10:50:18 +0100

    Updated build files

--- RestrictedNeat/setup.py
+++ RestrictedNeat/setup.py
@@ -7,5 +7,12 @@ setup(name='RestrictedNeat',
       author_email='regule@runbox.com',
       package_dir = {'': 'scripts'},
       packages=['resneat.backend', 'resneat.utils'],
+      install_requires=[
+          'clang==5.0',
+          'Box2D==2.3.10',
+          'pandas==1.3.3',
+          'pyneat==0.1.3',
+          'numpy==1.19.5'
+          ]
      )
 
