979ffa6c8784de2a87ee49ef5a54efd21bc0b1fe
regule <regule@runbox.com>
2021-12-11 10:46:13 +0100

    Added setup.py

---
+++ RestrictedNeat/scripts/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(name='RestrictedNeat',
+      version='0.2',
+      description='Restricted Neat',
+      author='Piotr Gnyś',
+      author_email='regule@runbox.com',
+      package_dir = {'': '.'},
+      packages=['rsneat',],
+      install_requires=[
+          'clang==5.0',
+          'Box2D==2.3.10',
+          'pandas==1.3.3',
+          'pyneat==0.1.3',
+          'numpy==1.19.5',
+          'gym==0.21.0',
+          'matplotlib==3.5.0',
+          'neat-python==0.92',
+          'pyserial==3.5'
+          ],
+      entry_points={
+          'console_scripts': ['restneat_test_classic_neat=resneat.tests.run_neat_demo:main']
+          },
+      setup_requires=['flake8']
+     )
+
