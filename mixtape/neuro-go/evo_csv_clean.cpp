eee56d52712529b96115f4263121705c8a723d60
Regule <regule@runbox.com>
2022-01-04 15:33:13 +0100

    Added dataset

--- mixtape/neuro-go/evo_csv_clean.cpp
+++ mixtape/neuro-go/evo_csv_clean.cpp
@@ -12,8 +12,17 @@ using std::endl;
 using std::cout;
 using std::cin;
 
+
+//-------------------------------------------------------------------------------------------------
+//                                   NUMERICAL VECTOR
+//-------------------------------------------------------------------------------------------------
+
 class NumericVector: public std::vector<float>{
 public:
+
+	NumericVector() : std::vector<float>(){
+	}
+
 	NumericVector(int size) : std::vector<float>(size,0){
 	}
 
@@ -40,6 +49,50 @@ std::ostream &operator<<(std::ostream &os, NumericVector const &num_vec){
     return os;
 }
 
+//-------------------------------------------------------------------------------------------------
+//                                      DATA SET 
+//-------------------------------------------------------------------------------------------------
+class Dataset : public std::vector<NumericVector>{
+private:
+	std::vector<string> feature_names;
+
+public:
+	Dataset(int observation_count, int feature_count) : std::vector<NumericVector>(observation_count){
+		feature_names = std::vector<string>(feature_count, "UNKNOWN");
+		for(int i=0; i<this->size(); i++){
+			this->at(i) = NumericVector(feature_count);
+		}
+	}
+
+	Dataset(const Dataset& src) : std::vector<NumericVector>(src){
+		feature_names = src.feature_names;
+	}
+
+	int get_feature_count() const{
+		return feature_names.size();
+	}
+
+	string get_feature_name(int i) const{
+		return feature_names[i];
+	}
+};
+
+std::ostream &operator<<(std::ostream &os, Dataset const &dataset){
+	os << dataset.size() << ' ' << dataset.get_feature_count() << endl;	
+	for(int i=0; i<dataset.get_feature_count(); i++){
+		os << dataset.get_feature_name(i) << ' ';
+	}
+	os << endl;
+	for(size_t i=0;  i<dataset.size(); i++){
+		os << dataset[i] << endl;
+	}	   
+    return os;
+}
+
+//-------------------------------------------------------------------------------------------------
+//                                  MAIN FUNCTION 
+//-------------------------------------------------------------------------------------------------
+
 int main(int agrc, char** argv){
 	NumericVector v(5);
 	cout<<v<<endl;
@@ -47,5 +100,7 @@ int main(int agrc, char** argv){
 	cin>>v;
 	cout<<"--------------------------"<<endl;
 	cout<<v<<endl;
-
+	cout<<"--------------------------"<<endl;
+	Dataset ds(10,4);
+	cout<<ds;
 }
