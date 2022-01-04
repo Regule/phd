e53186aa2081fd781169013276d968a2f2c418c6
Regule <regule@runbox.com>
2022-01-04 14:18:07 +0100

    Added numeric vector class

--- mixtape/neuro-go/evo_csv_clean.cpp
+++ mixtape/neuro-go/evo_csv_clean.cpp
@@ -4,15 +4,32 @@
 #include<iostream>
 #include<algorithm>
 #include<sstream>
-
+#include<vector>
 
 using std::string;
 using std::fstream;
 using std::endl;
 using std::cout;
+using std::cin;
 
 class NumericVector: public std::vector<float>{
+public:
+	NumericVector(int size) : std::vector<float>(size,0){
+	}
 
+	NumericVector(const NumericVector& src) : std::vector<float>(src){
+	}	
+
+	friend std::istream &operator>>(std::istream  &input, NumericVector &v) {
+		int size;
+		input >> size;
+		NumericVector tmp(size);
+		for(int i=0; i<size; i++){
+			input >> tmp[i];
+		}
+		v=tmp;
+		return input;
+	}
 };
 
 std::ostream &operator<<(std::ostream &os, NumericVector const &num_vec){
@@ -24,5 +41,11 @@ std::ostream &operator<<(std::ostream &os, NumericVector const &num_vec){
 }
 
 int main(int agrc, char** argv){
+	NumericVector v(5);
+	cout<<v<<endl;
+	cout<<"Enter custom vector"<<endl;
+	cin>>v;
+	cout<<"--------------------------"<<endl;
+	cout<<v<<endl;
 
 }
