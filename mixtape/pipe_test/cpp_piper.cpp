b630f88ced554fab6adad68377402c6afebef21e
Regule <regule@runbox.com>
2022-01-11 19:30:26 +0100

    :(

--- mixtape/pipe_test/cpp_piper.cpp
+++ mixtape/pipe_test/cpp_piper.cpp
@@ -3,13 +3,17 @@
 #include <string>
 
 int main(){
-	std::fstream pipe_in;
+	std::fstream pipe_in, pipe_out;
 	pipe_in.open("/tmp/python_to_cpp_pipe", std::ios::in);
+	pipe_out.open("/tmp/cpp_to_python_pipe", std::ios::out);
 	if (pipe_in.is_open()){ 
 		std::string tp;
-		while(getline(pipe_in, tp)){  
-			std::cout << tp << std::endl;
-		}
-		pipe_in.close();   
+		getline(pipe_in, tp); 
+		std::cout << tp << std::endl;
 	}
+	if(pipe_out.is_open()){
+		pipe_out << "This is from cpp" << std::endl;
+	}
+	pipe_in.close();   
+	pipe_out.close();
 }
--- mixtape/pipe_test/python_piper.py
+++ mixtape/pipe_test/python_piper.py
@@ -3,8 +3,13 @@ import sys
 
 def main():
     out_pipe = open("/tmp/python_to_cpp_pipe", "w")
+    in_pipe = open('/tmp/cpp_to_python_pipe', 'r')
     out_pipe.write('This is test')
+    line = in_pipe.readline()
     out_pipe.close()
+    print(line)
+    in_pipe.close()
+
 
 if __name__ == '__main__':
     main()
