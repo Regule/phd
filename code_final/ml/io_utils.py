1acbe624e17d087b99cae19b4d559add29aabb5e
Regule <regule@runbox.com>
2022-02-20 15:18:26 +0100

    Added besel train

--- code_final/ml/io_utils.py
+++ code_final/ml/io_utils.py
@@ -5,7 +5,8 @@ from time_utils import epoch_str_to_timestamp, timestamp_to_epoch_str
 
 def read_data_from_csv(csv, epoch_column, bias_column):
     clock_data = pd.read_csv(csv, sep=';')
-    clock_data[epoch_column] = clock_data[epoch_column].map(epoch_str_to_timestamp)
+    if type(clock_data[epoch_column][0]) == str:
+        clock_data[epoch_column] = clock_data[epoch_column].map(epoch_str_to_timestamp)
     x = clock_data.iloc[:][epoch_column].values
     y = clock_data.iloc[:][bias_column].values
     return x, y
