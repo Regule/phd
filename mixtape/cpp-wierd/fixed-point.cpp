771bb850a3a0c47f9c4d6ce4814b5963a411c3d1
Regule <regule@runbox.com>
2022-03-05 20:07:44 +0100

    Reworked chapters

--- mixtape/cpp-wierd/fixed-point.cpp
+++ mixtape/cpp-wierd/fixed-point.cpp
@@ -37,7 +37,12 @@ class Numeric{
 private:
 	uint32_t data;
 
+	static const uint32_t FLAG_MASK = 2139095040;
+	static const uint32_t TYPE_MASK = 2147483648;
+	static const size_t VALUE_SIZE = 23;
+
 public:
+
 	Numeric(){
 		this->data = 0;
 	}
@@ -47,7 +52,13 @@ public:
 	}
 	
 	char get_flag() const{
+		uint32_t flag = data | FLAG_MASK;
+		flag = flag >> VALUE_SIZE;
+		char *flag_bytes = reinterpreter_cast<char*>(&flag);
+		return flag_bytes[3];
 	}
+
+
 
 	static is_in_valid_range(double value){
 	}
