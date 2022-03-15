05e1b0b0d43d5940aea126af891f233bf91da8a5
Regule <regule@runbox.com>
2022-03-15 13:40:43 +0100

    Smt

--- mixtape/cpp-wierd/fixed-point.cpp
+++ mixtape/cpp-wierd/fixed-point.cpp
@@ -38,6 +38,12 @@ class Numeric{
 private:
 	uint32_t data;
 
+	// Minimum and maximum values in here do not include decimal point location an treat
+	// value as an integer.
+	static const uint32_t MAXIMUM_VALUE = 4194303;
+	static const uint32_t MINIMUM_VALUE = -4194304;
+	static const uint32_t MAXIMUM_VALUE_FLOATING = 1;
+	static const uint32_t MINIMUM_VALUE_FLOATING = -1;
 	static const uint32_t FLAG_MASK = 2139095040;
 	static const uint32_t TYPE_MASK = 2147483648;
 	static const size_t VALUE_SIZE = 23;
@@ -48,7 +54,18 @@ public:
 		this->data = 0;
 	}
 
-	Numeric(double d){
+	Numeric(double d, bool force_range){
+		if(value < MINIMUM_VALUE_FLOATING || value > MAXIMUM_VALUE_FLOATING){
+			if(force_range){
+				if(value > MAXIMUM_VALUE_FLOATING){
+					value = MAXIMUM_VALUE_FLOATING;
+				}else{
+					value = MINIMUM_VALUE_FLOATING;
+				}
+			}else{
+				throw OutOfRangeException(d,MINIMUM_VALUE,MAXIMUM_VALUE);
+			}
+		}
 
 	}
 	
@@ -62,6 +79,7 @@ public:
 
 
 	static bool is_in_valid_range(double value){
+		return 
 	}
 };
 
