f816fcb45aabd457ef0efa3c8729237f45a1fd83
Piotr Gnyś <regule@runbox.com>
2022-04-01 21:56:24 +0200

    More work on neat

--- Neat23/include/neat_core.h
+++ Neat23/include/neat_core.h
@@ -1,19 +1,27 @@
 
-
 template<class Numeric> class Link;
 template<class Numeric> class Node;
 
 
+/*! This enumeration informs about type of agregation operator of neuron.
+ * Agregation takes a vector of input signals and changes it into a scalar 
+ * value refered to as activation potential.
+ * All input signals are already passed trough links (akson + dendrite) 
+ * and as such multiplied by appropriate weights.*/
 enum AgregationType{
-	SUM,
-	PRODUCT,
-	MIN,
-	MAX
+	SUM, /*!< Signals are summed up.*/
+	PRODUCT, /*!< Signals are multipled.*/
+	MIN, /*!< Activation potential assumes value of smallest signal.*/
+	MAX /*!< Activation potential assumes value of greatest signal.*/
 };
 
+/*! This enumeration represents an activation function used by a neuron.
+ * Activation function takes neuron activation potential as input, substract
+ * bias value form it and then transforms result into a cell response.
+ */
 enum ActivationType{
-	LINEAR,
-	RECTIFIER,
+	LINEAR, /*!< Neuron returns its activation potential with bias substraced.*/
+	RECTIFIER, /*!< If activation potential is positive this acts like a linear function otherwise returns zero.*/
 	UNIPOLAR,
 	BIPOLAR
 };
