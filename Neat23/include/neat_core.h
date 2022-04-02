6e08483b03b915fa1cf5cf6bd21d610a435e4913
Piotr Gnyś <regule@runbox.com>
2022-04-02 14:59:26 +0200

    More on neat core

--- Neat23/include/neat_core.h
+++ Neat23/include/neat_core.h
@@ -3,9 +3,7 @@
  * algorithm operations. 
  * */
 #include<vector>
-
-
-
+#include <memory>
 
 
 /*! This enumeration informs about type of agregation operator of neuron.
@@ -43,3 +41,66 @@ enum NodeRole{
 
 template<class Numeric> class Link;
 template<class Numeric> class Node;
+
+
+template<class Numeric> class Link{
+private:
+	long genetic_marker;
+	Numeric weight;
+	std::shared_ptr< Node<Numeric> > target;
+	std::shared_ptr< Node<Numeric> > source;
+
+	static long last_genetic_marker;
+
+public:
+	Link(Numeric weight, std::shared_ptr< Node<Numeric> > target, std::shared_ptr< Node<Numeric> > source);
+	void pass_signal(Numeric signal, long cycle) const;
+	void mutate(double factor);
+	Numeric get_weight() const;
+	std::shared_ptr< Node<Numeric> > get_target() const;
+	std::shared_ptr< Node<Numeric> > get_source() const;
+
+	bool operator==(const Link<Numeric> &other); 
+	std::string to_string() const;
+};
+
+
+template<class Numeric> class Node{
+private:
+	long genetic_marker;
+	ActivationType activation;
+	AgregationType argregation;
+	NodeRole role;
+	std::vector< std::shared_ptr<Numeric> > outbound;
+	std::vector< std::shared_ptr<Numeric> > inbound;
+	long cycle;
+	Numeric activation_potential;
+	Numeric bias;
+
+	static long last_genetic_marker;
+
+public:
+	Node(ActivationType activation, AgregationType argregation, NodeRole role, Numeric bias);
+
+	void add_signal(Numeric signal);
+	void reset_activation_potential();
+	Numeric activate() const;
+
+	void increment_cycle();
+	bool is_current_cycle(long current_cyce);
+	void reset_cycle();
+
+	void mutate_activation();
+	void mutate_agregation();
+	void mutate_bias(double factor);
+
+	std::vector< std::shared_ptr<Numeric> > get_outbound_connections() const;
+	void connect_outgoing(std::shared_ptr< Link<Numeric> > link);
+	void connect_incoming(std::shared_ptr< Link<Numeric> > link);
+
+	bool operator==(const Node<Numeric> &other); 
+	std::string to_string() const;
+
+
+};
+
