b15443f4e978ecb5efd65f0c3c328319d801e2fc
Piotr Gnyś <regule@runbox.com>
2022-04-06 14:13:31 +0200

    More work

--- Neat23/include/numeric_types.h
+++ Neat23/include/numeric_types.h
@@ -1,6 +1,13 @@
+/*! This header contains all implementations of numeric variables are used by NEAT model
+ * in place of built-in implementations like double or integer.
+ */
 
 
-
+/*! This is an abstract class that provides all operations required from a numeric variable 
+ * by NEAT algorithm. It also guarantees that numeric type can be cast to double and can 
+ * be created from it. As for precision and range loss it is assumed to be :W
+ *
+ */
 class Numeric{
 
 
