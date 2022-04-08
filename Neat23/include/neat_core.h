53d6d45cd41469df761f7989942df26272d1c041
Regule <regule@runbox.com>
2022-04-08 08:42:30 +0200

    Wrote mutation config

--- Neat23/include/neat_core.h
+++ Neat23/include/neat_core.h
@@ -271,3 +271,53 @@ public:
 
 };
 
+
+/*! This structure describes mutation that changes vale represented by an enumeration.
+ * As such it can only switch between already defined values, for that fied called
+ * distribution was added. It provides probability distibution for occurence of each state.
+ */
+struct EnumeratedMutation{
+	double probability_of_occurence; /*!< Probability of mutation occuring */
+	std::vector<double> distribution; /*!< Probability distributuion for mutation results */
+};
+
+
+/*! Probabilities of topology changing mutation. Such mutation can add or remove element from
+ * network graph.
+ */
+struct TopologyMutation{
+	double deletion_probability; /*!< Probability of adding new element to model */
+	double addition_probability; /*!< Probability of removing element*/
+};
+
+/*! Data describing a mutation that changes value of a numeric value.
+ * Mutation result is calculated according to equation new=old+random(-1;1)*old*factor where 
+ * random(-1;1) represents a number chosen randomly with uniform distribution from inclusive range
+ * and old and new represent values of changed variable.
+ * Setting factor to zero generates value with equation new_bias=random(-1;1) ignoring old value. 
+ * All calculations are made with double precision floating point representation however final result is cast
+ * to numeric representation specified in class template. This may result in precision loss as well as in 
+ * range clamping.
+ */
+struct NumericMutation{
+	double probability_of_occurence; /*!< Probability of mutation occuring. */
+	double factor; /*!< Maximum factor by which value can change. */
+};
+
+
+/*! This structure holds all information required for executing mutation step.
+ * We distinguish three types of mutations. First is a mutation that changes topology
+ * of network. Those are most dramatic mutation as they can add or remove link or node.
+ * If node is removed all links assosciated with it will be also removed. 
+ * Second type of mutation is change in some enumerated value, in this case it will change
+ * to another valid enumeration based on probability distribution.
+ * Last type is numeric mutation which changes some numeric value within maximum factor given.
+ */
+struct MutationConfiguration{
+	TopologyMutation node; /*!< Addition or deletion of node */
+	TopologyMutation connection; /*!< Addition or deletion of link*/
+	EnumeratedMutation activation; /*!< Change of activation function in node*/
+	EnumeratedMutation agregation; /*!< Change of agregation function in node*/
+	NumericMutation weight; /*!< Change of weight value in connection.*/
+	NumericMutation bias; /*!< Change of bias value in node.*/
+};
