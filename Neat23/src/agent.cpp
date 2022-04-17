07dad6a66a410a2b9aa04e92943034f08f8c7e4e
Regule <regule@runbox.com>
2022-04-17 11:43:25 +0200

    Forgot about that

--- Neat23/src/agent.cpp
+++ Neat23/src/agent.cpp
@@ -9,6 +9,11 @@ Connection::Connection(Numeric weight): GenotypeDependant(){
 	this->weight = weight;
 }
 
+Connection::~Connection(){
+	if(source) source->disconnect_outgoing(*this);
+	if(target) target->disconnect_incoming(*this);
+}
+
 Connection::Connection(const Connection& source): GenotypeDependant(source){
 	this->weight = source.weight;
 }
@@ -16,3 +21,14 @@ Connection::Connection(const Connection& source): GenotypeDependant(source){
 void Connection::set_target(shared_ptr< Soma<Numeric>> target){
 	this->target = target;
 }
+
+void Connection::set_source(shared_ptr< Soma<Numeric>> source){
+	this->source = source;
+}
+
+Numeric Connection::pass_signal(Numeric signal) const{
+	return signal*weight;
+}
+
+
+
